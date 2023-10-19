a97b6dbe5aadb582ed28b1cd81525bdd709dfd2c
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2023-10-19 11:04:30 +0100

    update CustomHead.tsx and Customlayout.tsx

--- src/layouts/CustomHead.tsx
+++ src/layouts/CustomHead.tsx
@@ -1,10 +1,10 @@
 import Head from "next/head";
 
-const CustomHead = () => {
+const CustomHead = ({ title }: { title: string }) => {
   return (
     <Head>
-      <title>Oxbourn | Buisness Consulting</title>
-      <meta name="description" content="Oxbourn Consulting" />
+      <title>{title}</title>
+      <meta name="description" content="Oxbourn Consulting firm" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
--- src/layouts/Customlayout.tsx
+++ src/layouts/Customlayout.tsx
@@ -1,17 +1,35 @@
 import LargeWithNewsletter from "@/components/UI/Footer";
 import WithSubnavigation from "@/components/UI/Navbar";
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
 import React from "react";
 import CustomHead from "./CustomHead";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
-/**const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-  display: "swap",
-}); */
 const Customlayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
+  const pageTitles = {
+    home: "Home - Oxbourn Consulting",
+    about: "About Us - Oxbourn Consulting",
+  };
+
+  const handleRoutesTitle = () => {
+    let title = "";
+    switch (router.pathname) {
+      case "/":
+        title = pageTitles.home;
+        break;
+      case "/about":
+        title = pageTitles.about;
+        break;
+      default:
+        title = pageTitles.home;
+        break;
+    }
+    return title;
+  };
   return (
     <>
       <style jsx global>
@@ -21,7 +39,7 @@ const Customlayout = ({ children }: { children: React.ReactNode }) => {
           }
         `}
       </style>
-      <CustomHead />
+      <CustomHead title={handleRoutesTitle()} />
       <div className={inter.className}>
         <WithSubnavigation />
         <main>{children}</main>
